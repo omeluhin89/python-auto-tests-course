84bb71707629ed1e323202cc1f853f761b76164b
omelyuhin <omeluhin89@gmail.com>
2022-01-11 00:20:51 +0200

    add file for fixture's lessons

---
+++ section3_1/test_items.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_login_link(browser):
+    browser.implicitly_wait(5)
+    browser.get(link)
+    button = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.ID, "add_to_basket_form"))
+    )
